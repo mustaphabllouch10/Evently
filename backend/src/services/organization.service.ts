604c1a25e5228fa6819360eed6a9177f28e7819a
mustaphabllouch10 <mustaphabllouch10@gmail.com>
2026-09-14 11:21:21 +0100

    finishing the organization service

--- backend/src/services/organization.service.ts
+++ backend/src/services/organization.service.ts
@@ -1,6 +1,7 @@
 import Organization from "../models/organization.js";
 import type {
   CreateOrganizationInput,
+  UpdateOrganizationInput,
 } from "../types/organization.types.js";
 
 export const createOrganization = async (
@@ -16,3 +17,35 @@ export const createOrganization = async (
 
   return organization;
 };
+
+export const getOrganizations = async () => {
+  const organizations = await Organization.find();
+  return organizations;
+};
+
+export const getMyOrganizations = async (userId: string) => {
+  const organizations = await Organization.find({
+    members: userId,
+  });   
+  return organizations;
+};
+
+export const getOrganizationById = async (id: string) => {
+  const organization = await Organization.findById(id); 
+  return organization;
+};
+
+export const updateOrganization = async (
+  id: string,
+  data: Partial<UpdateOrganizationInput>
+) => {
+  const organization = await Organization.findByIdAndUpdate(id, data, {
+    new: true,
+  });
+  return organization;
+};
+
+export const deleteOrganization = async (id: string) => {
+  const organization = await Organization.findByIdAndDelete(id);
+  return organization;
+}
